faa6fdc79355906b182557f223eb157b9c65b669
Quang Huy <ltqhuy.th0112@gmail.com>
2023-01-05 15:18:49 +0700

    update Ui search

--- src/Pages/Search/Search.jsx
+++ src/Pages/Search/Search.jsx
@@ -3,22 +3,28 @@ import '../../assets/css/pages/search.scss'
 import React from 'react'
 
 const Search = () => {
-  const { Search } = Input
   const onSearch = (value) => {
     console.log(value)
   }
   return (
-    <div className='search w-50 p-4'>
-      <span className='title'>Search</span>
-      <Search
-        style={{ borderRadius: '5px' }}
-        placeholder="input search text"
-        allowClear
-        enterButton="Search"
-        size="middlex"
-        onSearch={onSearch}
-      />
-    </div>
+    <>
+      <div className='search p-4'>
+        <span className='title ps-2'>Search</span>
+        <Input.Search
+          className='input-form-login'
+          style={{ borderRadius: '5px' }}
+          placeholder="Product name"
+          allowClear
+          enterButton="Search"
+          size="middlex"
+          onSearch={onSearch}
+        />
+      </div>
+      <div className="title-component my-3">
+        <h1>Search result</h1>
+      </div>
+    </>
+
   )
 }
 
