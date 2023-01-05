aea21b69499d4591690d4ab425a4fbf2f3144b67
Quang Huy <ltqhuy.th0112@gmail.com>
2023-01-05 12:21:18 +0700

    add Ui page search

--- src/Pages/Search/Search.jsx
+++ src/Pages/Search/Search.jsx
@@ -1,8 +1,24 @@
+import { Input } from 'antd'
+import '../../assets/css/pages/search.scss'
 import React from 'react'
 
 const Search = () => {
+  const { Search } = Input
+  const onSearch = (value) => {
+    console.log(value)
+  }
   return (
-    <div>Search</div>
+    <div className='search w-50 p-4'>
+      <span className='title'>Search</span>
+      <Search
+        style={{ borderRadius: '5px' }}
+        placeholder="input search text"
+        allowClear
+        enterButton="Search"
+        size="middlex"
+        onSearch={onSearch}
+      />
+    </div>
   )
 }
 
